b320f3ba90ca7e29bd99b55afc4c89b0f62ff97c
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-03-30 20:06:42 -0500

    event binding

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -9,9 +9,24 @@ export class AppComponent {
   title = 'basics';
   imageUrl = 'https://source.unsplash.com/random';
   showImage = true;
-  progress = 54;
+  currencyValue = 2345;
+  inputValue = '';
 
   toggleImage() {
     this.showImage = !this.showImage;
+  }
+
+  increaseCurrencyValue() {
+    this.currencyValue++;
+  }
+
+  decreaseCurrencyValue() {
+    this.currencyValue--;
+  }
+
+  // Event Binding
+  onKeyUp(event: KeyboardEvent) {
+    const element = event.target as HTMLInputElement;
+    this.inputValue = element.value;
   }
 }
