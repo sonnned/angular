3e2b1b2fbe27cfb7984f864d38db465c27bc09dd
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-03-31 10:04:18 -0500

    Defining initial state for the the product in the component

--- M1/angular-store/src/app/components/product/product.component.ts
+++ M1/angular-store/src/app/components/product/product.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
+  @Input() product!: Product
 
 }
